b366c5b100645f5fe5a469303a88fbe62dd47dad
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-10-21 11:57:17 +0200

    docker compose and env files

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, ConfigModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
