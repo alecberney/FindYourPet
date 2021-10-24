808992df23de9a76953043aaa50da82b3e04c038
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-10-24 16:19:11 +0200

    Connection to db

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dotenv } from 'dotenv/config';
 
 @Module({
-  imports: [CatsModule, ConfigModule.forRoot()],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: 'database/.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.APP_DB_USER,
+      password: process.env.APP_DB_PASS,
+      database: process.env.APP_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
